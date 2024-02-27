bec60c889cdea6fcbdaf88a5ffc16bd9548c99dc
ронин <49647614+ronin49@users.noreply.github.com>
2024-02-27 14:03:48 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -11,11 +11,19 @@ void load(const wchar_t* file)
     map[file] = new Gdiplus::Image(file);
 }
 
+const int CELL_SIZE = 50;
 void draw(HDC dc, const wchar_t* file, int x, int y)
 {
-    Gdiplus::Rect rect(x, y, 50, 50);
+    Gdiplus::Rect rect(x, y, CELL_SIZE, CELL_SIZE);
     Gdiplus::Graphics grpx(dc);
     grpx.DrawImage(map[file], rect);
+}
+
+const wchar_t* field[8][8] = {};
+void drawField(HDC dc) {
+    for (int row = 0; row < 8; ++row)
+        for (int col = 0; col < 8; ++col)
+            draw(dc, field[row][col], col * CELL_SIZE, row * CELL_SIZE);
 }
 
 // Window Procedure function declaration
@@ -41,7 +49,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         WS_OVERLAPPEDWINDOW,            // Window style
 
         // Size and position
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        CW_USEDEFAULT, CW_USEDEFAULT, 9 * CELL_SIZE, 9 * CELL_SIZE,
 
         NULL,       // Parent window
         NULL,       // Menu
@@ -67,6 +75,35 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     load(L"black_horse.png");
     load(L"black_tura.png");
     load(L"black_peshka.png");
+    load(L"white_king.png");
+    load(L"white_queen.png");
+    load(L"white_slon.png");
+    load(L"white_horse.png");
+    load(L"white_tura.png");
+    load(L"white_peshka.png");
+
+    for (int col = 0; col != 8; ++col) {
+        field[1][col] = L"black_peshka.png";
+        field[6][col] = L"white_peshka.png";
+    }
+
+    field[0][0] = L"black_tura.png";
+    field[0][7] = L"black_tura.png";
+    field[0][1] = L"black_horse.png";
+    field[0][6] = L"black_horse.png";
+    field[0][2] = L"black_slon.png";
+    field[0][5] = L"black_slon.png";
+    field[0][3] = L"black_queen.png";
+    field[0][4] = L"black_king.png";
+
+    field[7][0] = L"white_tura.png";
+    field[7][7] = L"white_tura.png";
+    field[7][1] = L"white_horse.png";
+    field[7][6] = L"white_horse.png";
+    field[7][2] = L"white_slon.png";
+    field[7][5] = L"white_slon.png";
+    field[7][3] = L"white_queen.png";
+    field[7][4] = L"white_king.png";
 
     // Run the message loop
     MSG msg = {};
@@ -89,8 +126,8 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
     case WM_PAINT: {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
-        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-        draw(hdc, L"black_peshka.png",100,100);
+        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 2));
+        drawField(hdc);
         EndPaint(hwnd, &ps);
         return 0;
     }
